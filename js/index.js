8d1ba0794bb6ea4d02c53e4944e337b0c29b9059
wang-zhi-hua <18335833198@sina.cn>
2016-04-27 09:05:46 +0800

    1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,23 @@
-$(function(){
+jQuery(document).ready(function($) {
 	$('.banner').height($(window).height());
 	$('.ck-banner').height($(window).height());
+
+    //跳转    
+    $(".nav1 ul li a").click(function(event) { 
+        var index=this.title
+        var id='#'+'index_'+index
+        $("html,body").animate({scrollTop: $(id).offset().top-70}, 1000);
+    });
+    //下拉搜索框
+	$(function() {  
+	    $(window).scroll(function(){
+	    t = $(document).scrollTop();
+	    if(t>500){
+	      $('.hd').show();
+	    }else{
+	      $('.hd').hide();
+	    }     
+	    })
+    });
+    //3D轮播
 })
